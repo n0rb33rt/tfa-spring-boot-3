8febbe72ee112ce8dec5a1663637d70b6eec89da
Norbert <norbert.tovt.pz.2022@lpnu.ua>
2023-08-24 13:24:31 +0300

    The innitial suite for building the project

---
+++ src/main/java/com/norbert/tfa/TfaApplication.java
@@ -0,0 +1,13 @@
+package com.norbert.tfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TfaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/norbert/tfa/TfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.norbert.tfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
